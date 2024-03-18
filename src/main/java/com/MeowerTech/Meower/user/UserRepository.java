9dbde218a1936c8def5831d3ebeb7ca443cee6c8
Ali najafian <alinajafian580@gmail.com>
2024-03-18 16:20:12 +0330

    New findByName in userRepo

--- src/main/java/com/MeowerTech/Meower/user/UserRepository.java
+++ src/main/java/com/MeowerTech/Meower/user/UserRepository.java
@@ -7,7 +7,11 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    //All Good
+
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByName(String name);
 }
