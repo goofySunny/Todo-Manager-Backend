46cc8807380f4714f5e6ee32f7d6d59f57402449
Ali najafian <alinajafian580@gmail.com>
2024-05-15 14:56:28 +0330

    clean up

--- src/main/java/com/MeowerTech/Meower/config/SecurityConfig.java
+++ src/main/java/com/MeowerTech/Meower/config/SecurityConfig.java
@@ -5,15 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-// @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
