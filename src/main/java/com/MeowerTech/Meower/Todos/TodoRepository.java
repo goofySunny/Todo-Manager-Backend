9f8782092d902c5a81991ab6a6c27b25e284af80
Ali <Alinajafian580@Gmail.com>
2025-03-06 14:10:55 +0330

    removed redundant method (it was included in the default package)

--- src/main/java/com/MeowerTech/Meower/Todos/TodoRepository.java
+++ src/main/java/com/MeowerTech/Meower/Todos/TodoRepository.java
@@ -1,10 +1,7 @@
 package com.MeowerTech.Meower.Todos;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import com.MeowerTech.Meower.user.User;
 
@@ -12,7 +9,4 @@ import com.MeowerTech.Meower.user.User;
 public interface TodoRepository extends JpaRepository<TodoModel, String> {
 
     List<TodoModel> findAllByUser(User user);
-
-    @org.springframework.lang.NonNull
-    Optional<TodoModel> findById(@NonNull String id);
 }
