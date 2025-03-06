842219bf4d3667d1213f92bd193567f5f6f63690
Ali <Alinajafian580@Gmail.com>
2025-03-06 13:48:09 +0330

    RequiredArgsConstructor instead of constructor injection

--- src/main/java/com/MeowerTech/Meower/Auth/AuthenticationController.java
+++ src/main/java/com/MeowerTech/Meower/Auth/AuthenticationController.java
@@ -1,23 +1,20 @@
 package com.MeowerTech.Meower.Auth;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/auth")
 public class AuthenticationController {
 
     private final AuthenticationService service;
-
-    @Autowired
-    public AuthenticationController(AuthenticationService service) {
-    	this.service = service;
-    }
     
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
