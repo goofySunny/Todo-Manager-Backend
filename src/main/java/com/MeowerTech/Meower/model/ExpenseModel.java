d7d090bb9f3bb3c30f9bb883ced279bc9e6a24b5
Ali <Alinajafian580@Gmail.com>
2025-03-10 17:58:31 +0330

    Minor fixes for jackson and relation to user class

--- src/main/java/com/MeowerTech/Meower/model/ExpenseModel.java
+++ src/main/java/com/MeowerTech/Meower/model/ExpenseModel.java
@@ -1,11 +1,14 @@
 package com.MeowerTech.Meower.model;
 
 import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,6 +26,10 @@ public class ExpenseModel {
     private String id;
     private double amount;
     private LocalDate dateOfAddition;
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
 
 }
